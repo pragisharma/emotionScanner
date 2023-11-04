34d09c89d9ca218b8d0b39d0135e4980207cab7a
Pragya Sharma <emailpragyasharma@gmail.com>
2023-11-04 15:46:42 -0700

    processing img

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
+import cv2
+import matplotlib.pyplot as plt
+from deepface import DeepFace
+from config import Config
+
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 # @app.route("/")
 # def hello_world():
 #     return 'Hello World!'
+
+
+## @app.route('/submit', methods-['POST'])
 
 
 @app.route('/')
@@ -12,6 +21,20 @@ def index():
     return render_template('file_upload.html')
 
 
+def process(location):
+    img = cv2.imread(location)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faceCascade = cv2.CascadeClassifier('haarcascades_frontalface_default.xml')
+    faces = faceCascade.detectMultiScale(gray, 1.1, 4)
+
+    for(x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    predictions =  DeepFace.analyze(img)
+    predictions = predictions[0]
+    print(predictions)
+    dominant_emotion = predictions['dominant_emotion']
+    return dominant_emotion
 
 
 
