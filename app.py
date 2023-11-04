71a1b5d13a85f08d2e639b7c175bc21407134ea5
Pragya Sharma <emailpragyasharma@gmail.com>
2023-11-04 16:01:13 -0700

    added stored image route + cleaned up notebook

--- app.py
+++ app.py
@@ -3,26 +3,37 @@ import matplotlib.pyplot as plt
 from deepface import DeepFace
 from config import Config
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, send_from_directory
+
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
-
-# @app.route("/")
-# def hello_world():
-#     return 'Hello World!'
-
-
-## @app.route('/submit', methods-['POST'])
-
 
 @app.route('/')
 def index():
     ## /Users/pragya/Desktop/emotionScanner/templates
     return render_template('file_upload.html')
 
+@app.route('/storeimg', methods=['POST'])
+def store_img():
+    img_file = request.files['imgforminput']
+    # get file from HTML form
+    if img_file.filename == '':
+        return '<h1>NO FILE UPLOADED</h1>'
+    
+    filename = secure_filename(img_file.filename)
+    file_loc = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    img_file.save(file_loc)
+    eopt = process(file_loc)
+    if(eopt == "happy") :
+        m = "wow that's good" 
+    
+    return render_template('output.html', emotion_output=eopt, message=m)   
 
-def process(location):
-    img = cv2.imread(location)
+
+def process(loc):
+    img = cv2.imread(loc)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faceCascade = cv2.CascadeClassifier('haarcascades_frontalface_default.xml')
     faces = faceCascade.detectMultiScale(gray, 1.1, 4)
